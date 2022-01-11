366c0c8b2a1cb200fd268df7483462d08e651882
vinit <vinitsxn@gmail.com>
2022-01-11 08:23:09 +0530

    Added: Welcome Message added

---
+++ src/deckofcards/DeckOfCards.java
@@ -0,0 +1,13 @@
+/**
+ * @Author: Vinit Kumar
+ * @Created on: 11-01-2022
+ * Deck of Cards
+ */
+package deckofcards;
+
+public class DeckOfCards {
+    public static void main(String[] args){
+        //Welcome message
+        System.out.println("Welcome to Deck of Cards Game!!!");
+    }
+}
